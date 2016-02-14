aa988d26a9dd89cec02f2b4865e198e62a83d5c9
Benedikt Arnold <ben@benarnold.de>
2016-02-14 22:05:33 +0100

    optional demo config with DEMO environment variable

--- fhem-entrypoint.sh
+++ fhem-entrypoint.sh
@@ -7,6 +7,9 @@ if [ ! -d $CONFIGDIR ]; then
   mkdir -p $CONFIGDIR/log
 fi
 
+if [ -n $DEMO ] ; then
+    cp /opt/fhem/fhem.cfg.demo $FHEM_CFG
+fi
 
 if [ ! -e "$FHEM_CFG" ] ; then
     cp /opt/fhem/fhem.cfg.template $FHEM_CFG
